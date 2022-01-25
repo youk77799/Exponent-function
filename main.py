17b9090b6575b7466e3a18acf0e116d38d407c50
youk77799 <63965544+youk77799@users.noreply.github.com>
2022-01-25 20:18:19 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+print(2**3)
+
+def raise_to_power(base_num, pow_num):
+    result = 1
+    for index in range(pow_num):
+        result = result * base_num
+    return result
+
+print(raise_to_power(3, 4))
+
+# If you get stuck, check out this link : https://www.youtube.com/watch?v=rfscVS0vtbw 2:41:20
